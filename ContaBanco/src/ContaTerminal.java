9e2bd82f9a6410b210e25a2402070f1a4a82edcf
San4si <san_juq@hotmail.com>
2023-09-04 18:33:32 -0300

    feat: Implementa coleta de dados do usuário e exibição de mensagem personalizada

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor digite o numero da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o numero da Conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " , 0brigado por criar uma conta em nosso banco!");
+
+        System.out.println("Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de R$" + saldo + ", já está disponível para saque.");
+        
+        scanner.close();
     }
 }
